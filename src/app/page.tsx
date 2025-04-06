bb01a445e5bc1c19d8f7ba225cedb3ae803ba144
sumamakhan7611 <khansumama87111@gmail.com>
2025-04-06 14:08:25 +0530

    version-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,10 @@ import {
   generateAnimationArray,
   sortingAlgorithmsData,
 } from "@/lib/utils";
+import { useState } from "react";
+import { BiInfoCircle } from "react-icons/bi";
 import { FaPlayCircle } from "react-icons/fa";
+import { FiBarChart2 } from "react-icons/fi";
 import { RxReset } from "react-icons/rx";
 
 export default function Home() {
@@ -42,18 +45,21 @@ export default function Home() {
       runAnimation
     );
   };
-
+  const [showInfo, setShowInfo] = useState(true)
   return (
-    <main className="absolute top-0 h-screen w-screen z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
+    <main className="absolute top-0 h-screen w-screen z-[-2] bg-gradient-to-br from-gray-900 via-purple-950 to-gray-900">
       <div className="flex h-full justify-center">
         <div
           id="content-container"
           className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4"
         >
           <div className="h-[66px] relative flex items-center justify-between w-full">
-            <h1 className="text-gray-300 text-2xl font-light hidden md:flex">
-              Sorting Visulizer
-            </h1>
+          <div className="flex items-center gap-2">
+              <FiBarChart2 className="h-8 w-8 text-purple-500" />
+              <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-sky-700">
+                Sorting Visualizer
+              </h1>
+            </div>
             <div className="flex items-center justify-center gap-4">
               <Slider
                 isDisabled={isSorting}
@@ -76,9 +82,15 @@ export default function Home() {
                   <FaPlayCircle className="text-system-green60 h-8 w-8" />
                 )}
               </button>
+              <button
+                onClick={() => setShowInfo(!showInfo)}
+                className="rounded-full bg-gray-700 hover:bg-gray-600 text-white border-0 mt-1"
+              >
+                <BiInfoCircle className="h-7 w-7" />
+              </button>
             </div>
-
-            <div className="hidden sm:flex absolute top-[120%] left-0 w-full">
+            
+            {showInfo && (<div className="hidden sm:flex absolute top-[120%] left-0 w-full">
               <div className="flex w-full text-gray-400 p-4 rounded border border-system-purple20 bg-system-purple80 bg-opacity-10 gap-6">
                 <div className="flex flex-col items-start justify-start w-3/4">
                   <h3 className="text-lg">
@@ -113,7 +125,8 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-            </div>
+            </div>)}
+
           </div>
           <div className="relative h-[calc(100vh-66px)] w-full">
             <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
